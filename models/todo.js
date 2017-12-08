57eca46f2ed8d387d614a7de7b347aec25dadbbf
Dimnat <dnatos87@gmail.com>
2017-12-07 18:59:54 -0800

    Add required at name

--- models/todo.js
+++ models/todo.js
@@ -3,7 +3,7 @@ var mongoose = require("mongoose");
 var todoSchema = new mongoose.Schema({
 	name: {
 		type: String,
-		required: "Name canot be blank"
+		required: "Name cannot be blank"
 	},
 	completed: {
 		type: Boolean,
